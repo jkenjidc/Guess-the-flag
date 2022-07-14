537f9f896efa20709393073fdf32b5ffe7f8a5cb
kenji dela cruz <kenji.delacruz@willowtreeapps.com>
2022-07-13 21:15:36 -0400

    Initial Commit

---
+++ GuessingFlag/GuessingFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingFlagApp.swift
+//  GuessingFlag
+//
+//  Created by Justine Kenji Dela Cruz on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessingFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
